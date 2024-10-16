e5382625b4e2710332ab1558160df1282db746c0
Rafael Oliveira <rafael.orafa@gmail.com>
2024-10-16 20:27:42 -0300

    Resolução QUestão S01-06

--- App.java
+++ App.java
@@ -1,3 +1,22 @@
-public class App {
+import java.util.Scanner;
 
+public class App {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite o raio do circulo: ");
+    double raio = sc.nextDouble();
+
+    sc.close();
+
+    // double area = 3.14 * raio * raio;
+
+    final double PI = 3.14;
+
+    double area = PI * Math.pow(raio, 2);
+
+    System.out.printf("ÁREA DO CÍRCULO %.2f", area);
+
+
+    }
 }
